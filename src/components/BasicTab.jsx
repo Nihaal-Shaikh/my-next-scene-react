8cf5ed9fdaa315c88433be83d4dd5df99ec9f7f6
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2024-02-25 14:29:09 +0100

    Rating and language dropdown displayed

--- src/components/BasicTab.jsx
+++ src/components/BasicTab.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import BasicRadioButtons from './BasicRadioButtons';
 import BasicCheckboxes from './BasicCheckboxes';
 import BasicTimePeriod from './BasicTimePeriod';
+import ImdbRating from './ImdbRating';
+import LanguageDropdown from './LanguageDropdown';
 
 const BasicTab = () => {
 
@@ -10,6 +12,8 @@ const BasicTab = () => {
             <BasicRadioButtons />
             <BasicCheckboxes />
             <BasicTimePeriod />
+            <ImdbRating />
+            <LanguageDropdown />
             <button>Search</button>
         </>
     );
